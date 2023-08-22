d7d5571aa9c55f57be28331fef3e48c6330f7d80
Tereza Smolová <framboiseart@email.cz>
2023-08-22 12:41:21 +0200

    Repair

--- script.js
+++ script.js
@@ -16,12 +16,10 @@ document.querySelector('.check').addEventListener('click', function () {
 
     // when there is no input
     if (!guess) {
-        // document.querySelector('.message').textContent = '⛔ No number!';
         displayMessage('⛔ No number!')
 
     // when player wins
     } else if (guess === secretNumber) {
-        // document.querySelector('.message').textContent = '🎉 Correct Number!';
         displayMessage('🎉 Correct Number!')
         document.querySelector('.number').textContent = secretNumber;
 
@@ -36,46 +34,20 @@ document.querySelector('.check').addEventListener('click', function () {
     // when quess is wwrong
     } else if (guess !== secretNumber) {
         if (score > 1) {
-            // document.querySelector('.message').textContent = guess > secretNumber ? '📈 Too hight' : '📉 Too low';
             displayMessage(guess > secretNumber ? '📈 Too hight' : '📉 Too low')
             score--;
             document.querySelector('.score').textContent = score;
         } else {
-            // document.querySelector('.message').textContent = '🔚 You lost the game!';
             displayMessage('🔚 You lost the game!')
             document.querySelector('.score').textContent = 0;
         }
     }
-
-    // // when guess is too high
-    // } else if (guess > secretNumber) {
-    //     if (score > 1) {
-    //         document.querySelector('.message').textContent = '📈 Too hight';
-    //         score--;
-    //         document.querySelector('.score').textContent = score;
-    //     } else {
-    //         document.querySelector('.message').textContent = '🔚 You lost the game!';
-    //         document.querySelector('.score').textContent = 0;
-    //     }
-
-    // // when guess is too low
-    // } else if (guess < secretNumber) {
-    //     if (score > 1) {
-    //         document.querySelector('.message').textContent = '📉 Too low';
-    //         score--;
-    //         document.querySelector('.score').textContent = score;
-    //     } else {
-    //         document.querySelector('.message').textContent = '🔚 You lost the game!';
-    //         document.querySelector('.score').textContent = 0;
-    //     }
-    // }
 });
 
 document.querySelector('.again').addEventListener('click', function () {
     score = 20;
     secretNumber = Math.trunc(Math.random() * 20) + 1;
 
-    // document.querySelector('.message').textContent = 'Start guessing...';
     displayMessage('Start guessing...')
     document.querySelector('.score').textContent = score;
     document.querySelector('.number').textContent = '?';
